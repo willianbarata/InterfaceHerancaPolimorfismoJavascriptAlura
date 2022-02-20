22004640c74318fbdd9675c3bc02260399d96749
willianbarata <willianbarata@gmail.com>
2022-02-19 21:54:15 -0300

    Aula 3 - Validações sobre tipo de conta

--- Conta.js
+++ Conta.js
@@ -1,15 +1,39 @@
 export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+    constructor(tipo,saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+        this._tipo = tipo;
     }
 
-    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }   
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
+        let valor = 1;
+        if(this._tipo == "corrente"){
+            valor = 1.1;
+        }
+        if(this._tipo == "salario"){
+            valor = 1.05;
+        }
+        if(this._tipo == "empresarial"){
+            valor = 1.15;
+        }
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
             return valor;
         }
     }
@@ -22,6 +46,9 @@ export class Conta{
     }
 
     transferir(valor, conta){
+        if(this._tipo == "salario"){
+           return;
+        }
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
         conta.novo = "novo";
