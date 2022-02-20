407c2c460d45cdf45b4b6b467a797b41c57fa5e3
willianbarata <willianbarata@gmail.com>
2022-02-19 21:45:20 -0300

    Aula 2 - Utilizando apenas uma conta - problema

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { Cliente } from "./Cliente.js"; 
 import { ContaCorrente } from "./ContaCorrete.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122211107);
 
-const conta1 = new ContaCorrente(cliente1, 1001);
+const conta1 = new Conta(0,cliente1, 1001);
 conta1.depositar(500);
 conta1.sacar(100);
 
-const contaPoupanca = new ContaPoupanca(100, cliente1, 1001);
+const contaPoupanca = new Conta(100, cliente1, 1001);
 console.log(contaPoupanca);
 console.log(conta1);
